c914adddfc9e9143119be4929762b91b12da4fed
Manoel Cyreno <manoel.cyreno@liferay.com>
2017-09-13 14:59:59 -0300

    Take screenshot when test fail

--- src/functionalTest/java/com/liferay/samples/functional/test/steps/SettingsStartingEndingSteps.java
+++ src/functionalTest/java/com/liferay/samples/functional/test/steps/SettingsStartingEndingSteps.java
@@ -2,9 +2,13 @@ package com.liferay.samples.functional.test.steps;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import com.liferay.gs.testFramework.UtilsKeys;
 import com.liferay.samples.functional.test.utils.CommonMethods;
 
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
@@ -25,8 +29,12 @@ public class SettingsStartingEndingSteps {
 //	}
 
 	@After
-	public void afterScenario() {
+	public void afterScenario(Scenario scenario) {
+		if (scenario.isFailed()) {
+			byte[] screenshot = ((TakesScreenshot) UtilsKeys.DRIVER).getScreenshotAs(OutputType.BYTES);
+			scenario.embed(screenshot, "image/png");
+		}
+
 		UtilsKeys.DRIVER.navigate().to(UtilsKeys.getUrlToHome() + UtilsKeys.getLinkToLogOut());
 	}
-
 }
